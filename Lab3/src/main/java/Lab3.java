d7fca55608f6b6d21b2ede6141255d95954ab225
stepanbog <step300699@yandex.ru>
2019-11-27 17:42:07 +0300

    Scripted auto-commit on change (2019-11-27 17:42:07) by gitwatch.sh

--- Lab3/src/main/java/Lab3.java
+++ Lab3/src/main/java/Lab3.java
@@ -5,5 +5,7 @@ public class Lab3 {
     public static void main (String[] args){
         SparkConf conf = new SparkConf().setAppName("Lab3");
         JavaSparkContext sc = new JavaSparkContext(conf);
+
+        
     }
 }
