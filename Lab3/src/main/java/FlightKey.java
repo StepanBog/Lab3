7024e533ee4504264fe3009bbcfda5f53827cc74
stepanbog <step300699@yandex.ru>
2019-11-27 21:28:53 +0300

    Scripted auto-commit on change (2019-11-27 21:28:53) by gitwatch.sh

--- Lab3/src/main/java/FlightKey.java
+++ Lab3/src/main/java/FlightKey.java
@@ -1,3 +1,10 @@
 public class FlightKey {
 
+    private final double Delay;
+    private final int Cancelled;
+
+    public FlightKey(String s, String s1) {
+        Delay = Double.parseDouble(s);
+        Cancelled = Integer.parseInt(s1);
+    }
 }
