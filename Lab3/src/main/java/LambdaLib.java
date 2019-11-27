a01035d0c37409fc8fc39a3f073bad69d40e53f9
stepanbog <step300699@yandex.ru>
2019-11-27 18:04:48 +0300

    Scripted auto-commit on change (2019-11-27 18:04:48) by gitwatch.sh

--- Lab3/src/main/java/LambdaLib.java
+++ Lab3/src/main/java/LambdaLib.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class LambdaLib {
     private static final CharSequence FLIGHTSTITLE = "\"YEAR\"" ;
     private static final String FLIGHTSSPLIT = ",";
-    private static final CharSequence AIRPORTSTITLE = ;
+    private static final CharSequence AIRPORTSTITLE = "Code";
     private static final String AIRPORTSSPLIT = ;
 
     public static JavaRDD<String[]> parseFlights(JavaRDD<String> flightsTable) {
