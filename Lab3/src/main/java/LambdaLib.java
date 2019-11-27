7c64360bfd14ca19d8ff843a6833751870d9baec
stepanbog <step300699@yandex.ru>
2019-11-27 17:58:02 +0300

    Scripted auto-commit on change (2019-11-27 17:58:02) by gitwatch.sh

--- Lab3/src/main/java/LambdaLib.java
+++ Lab3/src/main/java/LambdaLib.java
@@ -1,10 +1,12 @@
 import org.apache.spark.api.java.JavaRDD;
 
+import java.util.Arrays;
+
 public class LambdaLib {
     private static final CharSequence FLIGHTSTITLE = ;
 
     public static JavaRDD<String[]> parseFlights(JavaRDD<String> flightsTable) {
         return flightsTable.filter(s-> !s.contains(FLIGHTSTITLE))
-                .map(s-> Arr);
+                .map(s-> Arrays.stream());
     }
 }
