42a82263e5e75f27d2890a6c2e682d2fe8223273
stepanbog <step300699@yandex.ru>
2019-11-29 09:16:05 +0300

    Scripted auto-commit on change (2019-11-29 09:16:05) by gitwatch.sh

--- Lab3/src/main/java/LambdaLib.java
+++ Lab3/src/main/java/LambdaLib.java
@@ -43,6 +43,6 @@ public class LambdaLib {
     }
 
     public static JavaPairRDD<Tuple2<String, String>,FlightKey> reduce(JavaPairRDD<Tuple2<String, String>,FlightKey> id_id_delay_cancelled_pair) {
-        return id_id_delay_cancelled_pair.reduceByKey((FlightKey key1,FlightKey key2) ->( new FlightKey(Math.max(key1.delay,key2.delay),(key1.cancelledCount + key2.cancelledCount),(key1.delayedCount + key2.delayedCount),(key1.count + key2.count))));
+        return id_id_delay_cancelled_pair.reduceByKey(((FlightKey key1,FlightKey key2) ->( new FlightKey(Math.max(key1.delay,key2.delay),(key1.cancelledCount + key2.cancelledCount),(key1.delayedCount + key2.delayedCount),(key1.count + key2.count))));
     }
 }
