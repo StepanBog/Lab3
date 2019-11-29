90ea8e70e8b1fe71d67c1a6909e5b3cd9e35a078
stepanbog <step300699@yandex.ru>
2019-11-29 22:31:05 +0300

    Scripted auto-commit on change (2019-11-29 22:31:05) by gitwatch.sh

--- Lab3/src/main/java/LambdaLib.java
+++ Lab3/src/main/java/LambdaLib.java
@@ -30,7 +30,7 @@ public class LambdaLib {
                         .toArray(String[]::new));
     }
 
-    
+
     public static JavaPairRDD<Tuple2<String,String>,FlightKey> pair_ID_ID_Delay_Cancelled(JavaRDD<String[]> parsedFlights) {
         return parsedFlights.mapToPair(s-> new Tuple2<>(new Tuple2<>(s[ORIGIN_ID],s[DECT_ID]),new FlightKey(s[DELAY],s[CANCELLED])));
     }
