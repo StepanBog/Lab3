d864eba10c75bb1e0a15d6f92c5788a57664a6eb
stepanbog <step300699@yandex.ru>
2019-11-29 08:55:41 +0300

    Scripted auto-commit on change (2019-11-29 08:55:41) by gitwatch.sh

--- Lab3/src/main/java/FlightKey.java
+++ Lab3/src/main/java/FlightKey.java
@@ -2,7 +2,7 @@ import java.io.Serializable;
 
 public class FlightKey implements Serializable {
 
-    public double delay;
+    public int delay;
     public int cancelledCount = 0;
     public int count = 0;
     public int delayedCount = 0;
@@ -10,12 +10,12 @@ public class FlightKey implements Serializable {
     public FlightKey(String s, String s1) {
         cancelledCount = Integer.parseInt(s1);
         if (cancelledCount != 0)
-            delay = Double.parseDouble(s);
+            delay = (int)Double.parseDouble(s);
         else
-            delay = 0.0;
+            delay = 0;
 
     }
-    public FlightKey(double s,  int s1, int s2, int s3) {
+    public FlightKey(int s,  int s1, int s2, int s3) {
         delay = s;
         cancelledCount = s1;
         delayedCount = s2;
